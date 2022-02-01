b08349618dbe0db2eef7b8c1bf9c2bd3a7d9b7d3
Latisha Vaughm <vic86rous@gmail.com>
2022-02-01 12:38:51 -0600

    added security dependencies

--- src/main/java/com/liftoff/cnvInvestments/controllers/UserController.java
+++ src/main/java/com/liftoff/cnvInvestments/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.liftoff.cnvInvestments.controllers;
 import com.liftoff.cnvInvestments.entity.User;
 import com.liftoff.cnvInvestments.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,5 +24,15 @@ public class UserController {
     @PostMapping({"/registerNewUser"})
     public User registerNewUser(@RequestBody User user) {
         return userService.registerNewUser(user);
+    }
+
+    @GetMapping({"/forFundManager"})
+    public String forFundManager() {
+        return "This URL is accessible to Fund Managers only";
+    }
+
+    @GetMapping({"/forInvestor"})
+    public String forInvestor() {
+        return "This URL is accessible to Investors";
     }
 }
